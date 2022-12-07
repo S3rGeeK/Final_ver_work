d759b8474b9a266032050fafdda1eb49c0c9d818
Serg <serg-sv13@mail.ru>
2022-12-07 19:32:32 +0300

    Добавлена задача

--- Final_ver_work/Program.cs
+++ Final_ver_work/Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк 
+// формирует массив из строк, длина которых меньше либо равна 3 символа.
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+// Примеры:
+// ["hello", "2", "world", ":)"]-> ["2",":>"]
+// ["1234", "1567", "-2", "computer science"] -> ["-2"]
+// ["Russia", "Denmark", "Kazan"] -> []
+
+Console.WriteLine("Введите количество элементов исходного массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string?[] array = GetArray(size);
+
+Console.WriteLine("Полученный массив: ");
+PrintArray(array);
+Console.WriteLine();
+
+int secondArrayLength = GetSecondArrayLength(array);
+
+string?[] secondArray = GetSecondArray(array);
+Console.WriteLine("Новый массив: ");
+PrintArray(secondArray);
+
+string?[] GetArray(int size)
+{
+    Console.WriteLine("Введите элементы массива:");
+    string?[] array = new string[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
+void PrintArray(string?[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i != array.Length - 1) Console.Write($"\"{array[i]}\", ");
+        else Console.Write($"\"{array[i]}\"");
+    }
+}
+
+int GetSecondArrayLength(string?[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]!.Length <= 3) count++;
+    }
+    return count;
+}
+
+string?[] GetSecondArray(string?[] array)
+{
+    string?[] secondArray = new string?[secondArrayLength];
+    int indexOfSecondArray = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]!.Length <= 3)
+        {
+            secondArray[indexOfSecondArray] = array[i];
+            indexOfSecondArray++;
+        }
+    }
+    return secondArray;
+}
